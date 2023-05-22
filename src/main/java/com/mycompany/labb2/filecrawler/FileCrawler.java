34e08f22f54733a03c8a1fb8497038993bd2abc4
Anders Myrman <anders.myrman@turnpuu.se>
2023-05-22 13:50:39 +0200

    Clean up

--- src/main/java/com/mycompany/labb2/filecrawler/FileCrawler.java
+++ src/main/java/com/mycompany/labb2/filecrawler/FileCrawler.java
@@ -1,82 +1,75 @@
 package com.mycompany.labb2.filecrawler;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileCrawler {
+  
+  // Prompts the user for a search string and searches files in the current directory and its subdirectories.
+  public static void main(String[] args) {
 
-/* Tasks:
- * [x] 1. Starta med meddelande som säger åt användare att skriva in sökord
- * [ ] 2. Om fil innehåller textsträng -> absolut sökväg skrivs ut på egen rad
- * [ ] 3. Om fil inte innehåller sträng -> ingenting skrivs ut
- * [ ] 4. Om fil eller katalog inte går att läsa -> error med fils/katalogs absoluta sökväg System.error
- */
+    // Create a Scanner object from the Scanner class by calling the constructor that accepts InputStream type to read input from the keyboard
+    // Uses the 
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter search string:");
+    String searchString = scanner.nextLine();
+    scanner.close();
 
-    public static void main(String[] args) {
+    File currentDirectory = new File(".");
+    searchFiles(currentDirectory, searchString);
+  }
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to FileCrawler." + "\n" + "Please enter term to search for:");
-        String searchTerm = scan.nextLine();
-        String startDir; // startDir / root --> noder --> löv -- (binära sökträd)
-        // ett "objekt" kan inte vara både mapp och fil samtidigt
+  /**
+   * Searches files in a given directory (and its subdirectories) for a given search string.
+   * 
+   * @param directory The directory to search in.
+   * @param searchString The search string to look for.
+   */
+  public static void searchFiles(File directory, String searchString) {
+    File[] files = directory.listFiles();
+    if (files == null) {
+      System.err.println("Cannot read directory: " + directory.getAbsolutePath());
+      return;
     }
 
-    // public static void scan = new 
-    // public static void searchFile = 
-    // Notes, handledning med Alvin 220923
-    // canRead, isFile/isDirectory -- fil/dir (bool)
-    // 1. rekursivt klättra ner i filträdet
-    //      - komma till en fil som är läsbar
-    // 2. söka igenom filer
-    // - Anropa searchFile i scan
-    // - Gå djupare i mappstruktur?
-    // - Söka igenom filer?
-    /* Mål:
-           - Strömmar / streams (input/output)
-           - Filer
-           - Felhantering / exceptions
-           - Rekursion
-        
-          Uppgiften går ut på att skriva ett simpelt lokalt sökprogram som tar in en textsträng från 
-          System.in och gör en sökning i katalogen där programmet körs, samt dess underkataloger, i 
-          alla filer för att se om den angivna textsträngen finns i filen.
-        
-          - Programmet ska starta med ett meddelande som säger åt användaren att skriva in 
-            ett sökord.
-        
-        import java.util.Scanner;
-        
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to FileCrawler. This program takes an input,
-        a search term, and uses it to search through the files in your current
-        as well as sub directories.
-
-        Please enter term to search for:");
-        String searchString = scanner.nextLine();
-        
-        
-          - Om filen innehåller textsträngen...
-        try
-        if (file contains searchString) {
-        
-        /*  ...så ska filens ABSOLUTA SÖKGVÄG -- hur hitta och skriva ut
-                            sökvägen? Metod för det? Googla det...
-     */
- /*
-        // skrivas ut på en egen 
-        // rad till System.out. 
-            System.out.println(filens sökväg);
+    for (File file : files) {
+      if (file.isDirectory()) {
+        searchFiles(file, searchString);
+      } else {
+        try {
+          if (containsString(file, searchString)) {
+            System.out.println(file.getAbsolutePath());
+          }
+        } catch (IOException e) {
+          System.err.println("Cannot read file: " + file.getAbsolutePath());
         }
-          
-          - Om filen inte innehåller textsträngen så ska ingenting skrivas ut för den filen.
-        else (file !contains searchString)
-            Skip output.
-        
-        catch (Exception e) // e är alltså en Exception var
-            System.err.println(); // Throw / output exception System.error ouput path
-          - Om filen (eller en katalog) inte går att läsa så ska ett felmeddelande som minst 
-            innehåller filens/katalogens absoluta sökväg skrivas ut till System.error. -- try catch?
-        
-     */
+      }
+    }
+  }
+
+  /**
+   * Determines whether a given file contains a given search string.
+   * 
+   * @param file The file to check.
+   * @param searchString The search string to look for.
+   * @return True if the file contains the search string, false otherwise.
+   * @throws IOException If an I/O error occurs.
+   */
+  // a boolean method named containsString and accepts  that returns true if the file contains the search string
+  public static boolean containsString(File file, String searchString) throws IOException {
+    // try-with-resources statement to automatically close the reader
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        if (line.contains(searchString)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
 }
